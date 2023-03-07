71e6d99a99ad852c01d17cc97eeffa2412f0c585
Yathartha Panigrahy <yatharth99@gmail.com>
2023-03-06 19:10:53 -0500

    trying

--- app/src/main/java/com/example/realcodriver/SummaryFragment.kt
+++ app/src/main/java/com/example/realcodriver/SummaryFragment.kt
@@ -14,6 +14,8 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import org.w3c.dom.Text
+import java.net.HttpURLConnection
+import java.net.URL
 
 class SummaryFragment : Fragment() {
 
